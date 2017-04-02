172de8b21ec8c32428c691a911993ce13d001fba
Kristina Chodorow <k.chodorow@gmail.com>
2017-04-02 09:57:16 -0400

    Parameterize template

--- src/main/java/com/meab/ui/UiServlet.java
+++ src/main/java/com/meab/ui/UiServlet.java
@@ -11,13 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class IndexServlet extends HttpServlet {
+public class UiServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String uri = request.getRequestURI();
+    // Starts with '/':
+    uri = uri.substring(1);
+    if (uri.isEmpty()) {
+      uri = "index";
+    }
+    String page = uri + ".ftlh";
+
     Configuration freemarkerConfig = StartupRegistrar.getFreemarkerConfig();
-    Template template = freemarkerConfig.getTemplate("index.ftlh");
+    Template template = freemarkerConfig.getTemplate("common.ftlh");
     try {
-      template.process(ImmutableMap.of(), response.getWriter());
+      template.process(ImmutableMap.of("page", page), response.getWriter());
     } catch (TemplateException e) {
       e.printStackTrace();
     }
